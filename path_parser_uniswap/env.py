c1c869d3634a7bc74c67314522a0175531df7537
Danila Belozarovich <belazproger@gmail.com>
2024-01-19 22:09:05 +0100

    path parser

--- path_parser_uniswap/env.py
+++ path_parser_uniswap/env.py
@@ -4,7 +4,5 @@ load_dotenv()
 
 JSON_RPC_PROVIDER = os.environ.get("JSON_RPC_PROVIDER")
 UNI_CLI_PATH = os.environ.get("UNI_CLI_PATH")
-SETTINGS_DB__DSN = os.environ.get("SETTINGS_DB__DSN")
 PATH_DB__DSN = os.environ.get("PATH_DB__DSN")
 SYNC_DB__DSN = os.environ.get("SYNC_DB__DSN")
-REDIS_DSN__INFO = os.environ.get("REDIS_DSN__INFO")
--- price_parser_uniswap/__main__.py
+++ price_parser_uniswap/__main__.py
@@ -1,113 +1,91 @@
-import asyncio
+import json
 import logging
-import re
-import shlex
 import subprocess
-from _decimal import Decimal
-from typing import Optional, NamedTuple, Callable, Any, Final
+import traceback
+from typing import Optional, Dict, List, Final
 
-import web3
 from eth_typing import ChecksumAddress
+from eth_utils import to_checksum_address
 from jinja2 import Template
 
-from abstract.path_chain import PathChain, CLIQuoteUniswap
-from inmemory_storage.sync_db.sync_db import sync_db
 from abstract.const import INSTRUMENTS_CONNECTIVITY, INSTRUMENTS
 from abstract.exchange import Exchange
-from abstract.instrument import ExchangeInstrument, DEXExchangeInstrumentParams, Instrument
+from abstract.instrument import ExchangeInstrument, Instrument
 from inmemory_storage.kv_db import kv_db
+from inmemory_storage.sync_db.sync_db import sync_db
 from inmemory_storage.tg_settings_db.tg_settings_db import tg_settings_db
-from message_broker.message_broker import message_broker
-from message_broker.topics.price import LastPriceMessage, InstrumentPrice, publish_price_topic
-from price_parser_uniswap.env import JSON_RPC_PROVIDER, UNI_CLI_PATH
+from p_parser_uniswap.const import CHAIN
+from p_parser_uniswap.env import UNI_CLI_PATH
 
 NAME = "UNIv3"
 
-web3_connection = web3.Web3(web3.HTTPProvider(JSON_RPC_PROVIDER))
-path_key = Template("{{pair}}_path_{{type}}")
-
-ansi_escape = re.compile(r'\x1B(?:[@-Z\\-_]|\[[0-?]*[ -/]*[@-~])|[ \t\n"]')
+setting_db: Final = tg_settings_db()
+path_db: Final = kv_db()
+sync_db: Final = sync_db()
 
 REQUIRED_INSTRUMENTS = tuple(k for k, v in INSTRUMENTS_CONNECTIVITY.items() if any(ei.exchange == Exchange.UNISWAP for ei in v))
 INSTRUMENT_ARGUMENTS = {INSTRUMENTS[ExchangeInstrument(Exchange.UNISWAP, i)].dex:i for i in REQUIRED_INSTRUMENTS}
 
-
-class FieldMapper(NamedTuple):
-    field_name: str
-    apply: Callable[[Any], Any]
+path_key = Template("{{pair}}_path_{{type}}")
 
 
-mapper = {
-    1: FieldMapper(
-        field_name="best_route",
-        apply=lambda x: x
-    ),
-    5: FieldMapper(
-        field_name="quote_in",
-        apply=lambda x: x
-    ),
-    7: FieldMapper(
-        field_name="gas_quote",
-        apply=lambda x: x[18:]
-    ),
-    8: FieldMapper(
-        field_name="gas_usd",
-        apply=lambda x: x[13:]
-    ),
-    9: FieldMapper(
-        field_name="call_data",
-        apply=lambda x: x[9:]
-    ),
-    12: FieldMapper(
-        field_name="block_number",
-        apply=lambda x: x[12:]
-    )
-}
-cli_height = range(13)
+def _quote(symbol: str, from_token: ChecksumAddress, to_token: ChecksumAddress, amount: int, type_: str, protocols: str) -> Optional[PathRow]:
+    command = f'{UNI_CLI_PATH}bin/cli quote-custom --tokenIn {from_token} --tokenOut {to_token} --amount {amount} --{type_} --recipient 0xAb5801a7D398351b8bE11C439e05C5B3259aeC9B --protocols {protocols}'
+    cli_result = subprocess.Popen(command, stdout=subprocess.PIPE, shell=True).communicate()
+    logging.info(f"{command}\n{cli_result}")
+    try:
+        output = cli_result[0].decode("utf-8").replace("\n", "")
+        if "Could not find route" in output:
+            return None
+        raw_pair_path = json.loads(output.strip())
+        token0_path_chain: TokenRow
+        token1_path_chain: TokenRow
+        from_token: ChecksumAddress
+        chains: Dict[str, List[PathRowChain]] = dict()
+        chain_list: List[PathRowChain]
+        for amount, raw_pair_pathes in raw_pair_path.items():
+            chain_list = list()
+            for raw_pair_path in raw_pair_pathes:
+                from_token = to_checksum_address(raw_pair_path.get("from_token"))
+                token0_path_chain = get_token_info_from_json(raw_pair_path.get("token0"), CHAIN)
+                token1_path_chain = get_token_info_from_json(raw_pair_path.get("token1"), CHAIN)
+                token0_path_chain, token1_path_chain = (token1_path_chain, token0_path_chain) if from_token == token1_path_chain.address else (token0_path_chain, token1_path_chain)
+
+                chain_list.append(PathRowChain(
+                    token_from=token0_path_chain,
+                    token_to=token1_path_chain,
+                    fee=raw_pair_path.get("fee")
+                ))
+            chains[amount] = chain_list
+        return PathRow(chains=chains)
+    except Exception:
+        logging.error(traceback.format_exc())
+        return None
 
 
-async def _quote(base: ChecksumAddress, quote: ChecksumAddress, amount: int, type_: str) -> Optional[CLIQuoteUniswap]:
-    command = f'{UNI_CLI_PATH}bin/cli quote -i {quote} -o {base} --amount {amount} --{type_} --recipient 0xAb5801a7D398351b8bE11C439e05C5B3259aeC9B --protocols v3'
-    args = shlex.split(command)
-    process = await asyncio.create_subprocess_exec(*args, cwd=UNI_CLI_PATH, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    quote = {}
-    for ind in cli_height:
-        line = await process.stdout.readline()
-        if line and mapper.get(ind):
-            quote[mapper[ind].field_name] = mapper[ind].apply(ansi_escape.sub('', line.decode("utf-8")))
-    return CLIQuoteUniswap(**quote)
+def _parse_path_info(instrument: Instrument, type_: str) -> Optional[PathRow]:
+    instrument.name
+    pair_row = info_rconn.get_pair_info(pair_symbol)
+    if type_ == "exactIn":
+        return _quote(pair_symbol, pair_row.token1, pair_row.token0, setting_db.get_setting().rvolume, type_, "v3")
+    return _quote(pair_symbol, pair_row.token0, pair_row.token1, setting_db.get_setting().rvolume, type_, "v3")
 
 
-async def main():
-    setting_db: Final = tg_settings_db()
-    path_db: Final = kv_db()
-    locker_db: Final = sync_db()
-    curr_block_number = 0
-    broker = await message_broker()
-    # await sender.connect()
-    while True:
-        if curr_block_number != connection.eth.block_number:
-            p: DEXExchangeInstrumentParams
-            i: Instrument
-
-            for p, i in INSTRUMENT_ARGUMENTS.items():
-                if not await locker_db.is_lock(i.value):
-                    await locker_db.lock_action(i.value, 60000)
-                    buy_path = PathChain.from_cli(await _quote(p.base.address, p.quote.address, 10000, "exactIn"))
-                    sell_path = PathChain.from_cli(await _quote(p.quote.address, p.base.address, 10000, "exactOut"))
-                    path_db.set(f'{i.value}exactIn', buy_path)
-                    path_db.set(f'{i.value}exactOut', sell_path)
-                    # await publish_price_topic(broker, LastPriceMessage(
-                    #     price=InstrumentPrice(buy=buy.quote_in, sell=sell.quote_in, buy_fee=buy.gas_usd, sell_fee=sell.gas_usd),
-                    #     exchange=Exchange.UNISWAP,
-                    #     instrument=i
-                    # ))
-
-            curr_block_number = connection.eth.block_number
+def _parse_path(pair_symbol: str, type_: str):
+    sync_key = path_key.render(pair=pair_symbol, type=type_)
+    if not sync_db.is_lock(sync_key):
+        sync_db.lock_action(sync_key, 60 * 2)
+        path = _parse_path_info(pair_symbol, type_)
+        # if path is None:
+        #     setting_rconn.deactivate_ex_pair(NAME, pair_symbol)
+        # else:
+        if path is not None:
+            path_db.set_exchange_pair_path(NAME, pair_symbol, path, type_)
 
 
 if __name__ == '__main__':
-    connection = web3.Web3(web3.HTTPProvider(JSON_RPC_PROVIDER))
     logging.basicConfig(level=logging.INFO)
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(main())
+    while True:
+        for instrument in REQUIRED_INSTRUMENTS:
+            _parse_path(instrument, "exactIn")
+            _parse_path(instrument, "exactOut")
